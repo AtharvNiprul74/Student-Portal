b37e71edfbdd5829248d41db0c113a047d7c6f95
AtharvNiprul74 <atharvniprul74@gmail.com>
2026-01-07 18:54:27 +0530

    Uploaded

--- Database/db.js
+++ Database/db.js
@@ -2,10 +2,13 @@ const mysql2 = require("mysql2"); //imported mysql2
 
 const pool = mysql2.createPool({
   //database connection
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = pool; //exported database
