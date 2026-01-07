de3e2720d1ae3ce33cb0662b15fca69b9132a610
AtharvNiprul74 <atharvniprul74@gmail.com>
2026-01-07 18:56:34 +0530

    Uploaded

--- Database/db.js
+++ Database/db.js
@@ -1,5 +1,12 @@
 const mysql2 = require("mysql2"); //imported mysql2
 
+console.log("DB CONFIG CHECK:", {
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+});
+
 const pool = mysql2.createPool({
   //database connection
   host: process.env.MYSQLHOST,
